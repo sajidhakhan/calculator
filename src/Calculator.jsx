28b0785dd17e0aa0ca16e0edf3d147db663c2066
sajidhakhan <81672634+sajidhakhan@users.noreply.github.com>
2024-08-05 14:08:32 +0530

    completed

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const calculator = () => {
-  return (
-    <div>calculator</div>
-  )
+const Calculator = () => {
+    const [input, setInput] = useState('');
+    const [result, setResult] = useState('');
+  
+    const handleButtonClick = (value) => {
+      setInput((prevInput) => prevInput + value);
+    };
+  
+    const handleClear = () => {
+      setInput('');
+      setResult('');
+    };
+  
+    const handleCalculate = () => {
+      try {
+        const evaluatedResult = eval(input);
+        if (isNaN(evaluatedResult)) {
+          setResult('NaN');
+        } else {
+          setResult(evaluatedResult.toString());
+        }
+      } catch (error) {
+        setResult('Error');
+      }
+    };
+  
+    return (
+      <div className="calculator-container">
+        <h1>React Calculator</h1>
+        <input type="text" className="calculator-input" value={input} readOnly />
+        { result &&<div className="result">{result}</div> }
+        <div className="buttons-container">
+          {['7', '8', '9', '/', '4', '5', '6', '*', '1', '2', '3', '-', '0', '.', '=', '+'].map((btn, index) => (
+            <button
+              key={index}
+              onClick={() => btn === '=' ? handleCalculate() : handleButtonClick(btn)}
+              className="calculator-button"
+            >
+              {btn}
+            </button>
+          ))}
+          <button onClick={handleClear} className="calculator-button">C</button>
+        </div>
+      </div>
+    );
 }
 
-export default calculator
+export default Calculator
